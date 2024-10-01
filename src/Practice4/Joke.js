608badc9cb95ed8d9b259c78fc6f944327f8b696
Bhoomi Gohil <26293804+BhoomiGohil@users.noreply.github.com>
2024-10-01 19:54:12 -0400

    Initial Comment

---
+++ src/Practice4/Joke.js
@@ -0,0 +1,8 @@
+export default function Joke(props) {
+  return (
+    <div>
+      {props.setup && <h3>Setup: {props.setup}</h3>}
+      {props.punchline && <p>Punchline: {props.punchline}</p>}
+    </div>
+  );
+}
